d50f3c5165a92b107bf8d28a7a7cb5364ef795f0
Kenneth McCord <kmccord@vt.edu>
2023-08-29 16:24:10 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class BookStoreForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        LoadButton = New Button()
+        SearchTextBox = New TextBox()
+        BookListBox = New ListBox()
+        DetailLabel = New Label()
+        SuspendLayout()
+        ' 
+        ' LoadButton
+        ' 
+        LoadButton.Location = New Point(297, 26)
+        LoadButton.Name = "LoadButton"
+        LoadButton.Size = New Size(119, 23)
+        LoadButton.TabIndex = 0
+        LoadButton.Text = "Load Books"
+        LoadButton.UseVisualStyleBackColor = True
+        ' 
+        ' SearchTextBox
+        ' 
+        SearchTextBox.Location = New Point(55, 26)
+        SearchTextBox.Name = "SearchTextBox"
+        SearchTextBox.Size = New Size(236, 23)
+        SearchTextBox.TabIndex = 1
+        ' 
+        ' BookListBox
+        ' 
+        BookListBox.FormattingEnabled = True
+        BookListBox.ItemHeight = 15
+        BookListBox.Location = New Point(55, 55)
+        BookListBox.Name = "BookListBox"
+        BookListBox.Size = New Size(361, 139)
+        BookListBox.TabIndex = 2
+        ' 
+        ' DetailLabel
+        ' 
+        DetailLabel.AutoSize = True
+        DetailLabel.Location = New Point(55, 210)
+        DetailLabel.Name = "DetailLabel"
+        DetailLabel.Size = New Size(127, 15)
+        DetailLabel.TabIndex = 3
+        DetailLabel.Text = "Addtional Book Details"
+        ' 
+        ' BookStoreForm
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(DetailLabel)
+        Controls.Add(BookListBox)
+        Controls.Add(SearchTextBox)
+        Controls.Add(LoadButton)
+        Name = "BookStoreForm"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents LoadButton As Button
+    Friend WithEvents SearchTextBox As TextBox
+    Friend WithEvents BookListBox As ListBox
+    Friend WithEvents DetailLabel As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,152 @@
+﻿Imports System.Data
+
+Imports Microsoft.VisualBasic.FileIO
+
+ 
+
+Public Class BookStoreForm
+
+ 
+
+    ' Declare DataTable to hold book data
+
+    Private bookData As New DataTable
+
+ 
+
+    Private Sub BookStoreForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Initialize DataTable columns
+
+        bookData.Columns.Add("Title")
+
+        bookData.Columns.Add("Author")
+
+        bookData.Columns.Add("Stock")
+
+    End Sub
+
+
+
+
+
+    ' Call procedure to load data from CSV into DataTable LoadButton.Click
+
+
+
+
+
+    ' Call procedure to filter the book list on SearchTextBox.TextChanged
+
+
+
+
+
+    ' Call procedure to display book details on BookListBox.SelectedIndexChanged
+
+
+
+
+
+    Private Sub LoadCSVData()
+
+        ' Clear any existing data
+
+        bookData.Clear()
+
+       
+
+        ' Initialize TextFieldParser and read CSV file
+
+        Using parser As New TextFieldParser("BookData.csv")
+
+            parser.TextFieldType = FieldType.Delimited
+
+            parser.SetDelimiters(",")
+
+            While Not parser.EndOfData
+
+                ' Read fields and add row to DataTable
+
+                Dim fields = parser.ReadFields()
+
+                bookData.Rows.Add(fields)
+
+            End While
+
+        End Using
+
+       
+
+        ' Fill the book list
+
+        FillBookList()
+
+    End Sub
+
+ 
+
+    Private Sub FillBookList()
+
+        ' Clear and populate the ListBox from DataTable
+
+        BookListBox.Items.Clear()
+
+        For Each row As DataRow In bookData.Rows
+
+            BookListBox.Items.Add(row("Title").ToString())
+
+        Next
+
+    End Sub
+
+ 
+
+    Private Sub FilterBookList()
+
+        ' Filter ListBox items based on search criteria
+
+        Dim filter = SearchTextBox.Text
+
+        BookListBox.Items.Clear()
+
+        For Each row As DataRow In bookData.Rows
+
+            If row("Title").ToString().Contains(filter) Then
+
+                BookListBox.Items.Add(row("Title").ToString())
+
+            End If
+
+        Next
+
+    End Sub
+
+ 
+
+    Private Sub DisplayBookDetails()
+
+        ' Display book details in DetailLabel
+
+        Dim selectedTitle = BookListBox.SelectedItem
+
+        If selectedTitle IsNot Nothing Then
+
+            For Each row As DataRow In bookData.Rows
+
+                If row("Title").ToString() = selectedTitle.ToString() Then
+
+                    DetailLabel.Text = $"Title: {row("Title")}" & vbCrLf & $"Author: {row("Author")}" & vbCrLf & $"Stock: {row("Stock")}"
+                    Exit For
+
+                End If
+
+            Next
+
+        End If
+
+    End Sub
+
+ 
+
+End Class
